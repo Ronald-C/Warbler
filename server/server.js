5f735108644eb518e993eafc8f1ab3f72bd71574
Ronald Cheng <ronaldcheng14@gmail.com>
2016-02-04 16:24:27 -0800

    Created server

---
+++ server/server.js
@@ -0,0 +1,14 @@
+// server.js
+
+var express = require("express");
+var app = express();
+var server = require("http").createServer(app);
+var io = require("socket.io")(server);
+
+// Router
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + "index.php");
+});
+
+server.listen(80, "127.1.1.1");
+
