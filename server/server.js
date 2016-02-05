fd1a09cb597bc0ce4ae57dbfa76a6cfe3d62420d
Hung Tran <hung_duy_tran@yahoo.com>
2016-02-04 16:27:00 -0800

    Server setup

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ var io = require("socket.io")(server);
 
 // Router
 app.get('/', function(req, res) {
-	res.sendFile(__dirname + "index.php");
+	res.sendFile(__dirname + "client/");
 });
 
 server.listen(80, "127.1.1.1");
