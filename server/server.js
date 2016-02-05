a7c91e4b4c7420f7ba5d3de587bb335c42164219
Hung Tran <hung_duy_tran@yahoo.com>
2016-02-04 16:43:06 -0800

    server setup to point to client

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
 // server.js
 
 var express = require("express");
+var path = require("path");
 var app = express();
 var server = require("http").createServer(app);
 var io = require("socket.io")(server);
 
 // Router
 app.get('/', function(req, res) {
-	res.sendFile(__dirname + "client/");
+	res.sendFile(path.join(__dirname, "../client", "index.html"));
 });
 
 server.listen(80, "127.1.1.1");
