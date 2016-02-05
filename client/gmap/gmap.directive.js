a801b2c52c60f68fca97b5a8e94d2e474afd13dc
Hung Tran <hung_duy_tran@yahoo.com>
2016-02-04 20:20:47 -0800

    Responsive styling for navbar

--- client/gmap/gmap.directive.js
+++ client/gmap/gmap.directive.js
@@ -2,7 +2,7 @@ function gmap_directive() {
 	return {
 		link: function($scope, $element, $attrs) {
 			var map = new google.maps.Map($element[0], {
-			    center: {lat: -34.397, lng: 150.644},
+			    center: {lat: 37.3360008, lng: -121.8847221},
 			    zoom: 8
 			});
 		}
