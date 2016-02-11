ff66a23a06b477e30eaf88b2d4a556dc31b2f811
Ronald Cheng <ronaldcheng14@gmail.com>
2016-02-11 14:23:23 -0800

    test code

--- server/twitter_factory.js
+++ server/twitter_factory.js
@@ -37,33 +37,66 @@ twitter_service.prototype = {
 
 	geoFetch: function(query, callback) {
 		var self = this;
-		self.client.get(
-			'search/tweets', 
-			{
-				q: decodeURI(query),
-				count: 100
-			},
-
-			function(error, tweets, response) {
-			if (error) {
-				console.log(error);
-				return;
-			}
-
-			var statuses = tweets.statuses;
-			var geos = [];
-			for (var i = 0, l = statuses.length; i < l; i++) {
-				var o = statuses[i];
-				if (o.geo) {
-					var twt = o.geo;
-					twt.text = o.text;
-					geos.push(twt);
-				}
-			}
-
-			(callback || noop)(geos);
-		});
+		
+		var geos = self.recurFetch(query);
+		console.log(geos);
+		(callback || noop)(geos);
 
 		return self;
+	},
+
+	recurFetch: function(query) {
+		var self = this;
+		var options = {
+			q: decodeURI(query),
+			count: 100
+		};
+
+		var geos = [];
+		self.client.get(
+			'search/tweets',
+			options,
+			function(error, tweets, response) {
+				if (error) {
+					console.log(error);
+					return;
+				}
+				var statuses = tweets.statuses;
+				options.max_id = statuses[statuses.length - 1].id;
+
+				for (var i = 0, l = statuses.length; i < l; i++) {
+					var o = statuses[i];
+					if (o.geo) {
+						var twt = o.geo;
+						twt.text = o.text;
+						geos.push(twt);
+					}
+				}
+				setTimeout(self.recurFetch, 5000);
+			}
+		);
+		return geos;
+	}
+}
+/****************************** HANDLERS *****************************/
+function decBy1(n) {
+	// Cast to a string; JS limited by 53bit
+	n = n.toString();
+	var allButLast = n.substr(0, n.length - 1);
+	var lastNum = n.substr(n.length - 1);
+
+	if(lastNum == "0") {
+		return decBy1(allButLast) + "9";
+	} else {
+		var result = allButLast + (parseInt(lastNum, 10) - 1).toString();
+		return trimLeft(result, "0");
+	}
+}
+
+function trimLeft(s, c) {
+	var i = 0;
+	// Remove leading "0"
+	while(i < s.length && s[i] === c) {
+		i++;
 	}
 }
