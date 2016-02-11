5861ce30485bd2c23b636097692014c2fec7be11
Hung Tran <hung_duy_tran@yahoo.com>
2016-02-11 00:34:28 -0800

    typeahead twitter applied. And now search queries will be passed to server as array not string

--- client/search/warblerSearch.directive.js
+++ client/search/warblerSearch.directive.js
@@ -1,14 +1,76 @@
 function warblerSearch_directive() {
+
+	var suggestions = new Bloodhound({
+		datumTokenizer: Bloodhound.tokenizers.whitespace,
+		queryTokenizer: Bloodhound.tokenizers.whitespace,
+		prefetch: "search/suggestions.json"
+	});
+
+	function fetchSuggestions(q, sync) {
+		if (q === '') {
+			sync(suggestions.get(
+				'earthquake', 'tornadoes', 'storm', 'snow', 'traffic'
+			));
+		} else {
+			suggestions.search(q, sync);
+		}
+	}
+
 	return {
 		templateUrl: 'search/warblerSearch.template.html',
 		link: function($scope, $element, $attrs) {
-			$element.find('.btnSearch').on('click', function() {
-				$element.find('form').submit();
-			});
+			function intentSubmission(query) {
+				if (query) $scope.searchInput = query;
+				if (!$scope.searchLog.hasOwnProperty(query) && typeof query !== 'undefined') {
+					$scope.searchLog[query] = true;
+				}
+
+				$scope.$emit('warblerSearch.searchInput.submit', Object.keys($scope.searchLog));
+			}
 
 			$element.find('form').on('submit', function(e) {
-				$scope.$emit('warblerSearch.searchInput.submit', $scope.searchInput);
+				intentSubmission();
 			});
-		}
+
+			$element.find('.inputSearch').typeahead({
+				hint: true,
+				highlight: true,
+				minLength: 0
+			}, {
+				name: 'states',
+				source: fetchSuggestions,
+				templates: {
+					empty: [
+						'<div class="text-muted">',
+						'No Suggestion',
+						'</div>'
+					].join('\n'),
+					suggestion: function(data) {
+						var templ = '<div class="list-group-item">' + '{{data}}</div>';
+
+						return templ.replace(/{{data}}/g, data);
+					}
+				}
+			});
+
+			$element.find('.inputSearch')
+				.on('focus', function() {
+					$(this).select();
+				})
+				.bind('typeahead:select', function(e, sugg) {
+					intentSubmission(sugg);
+				});
+
+			$element.find('#searchLog .dropdown-menu').on('click', function(e) {
+				e.stopPropagation();
+			});
+		},
+
+		controller: ['$scope', function($scope) {
+			$scope.searchLog = {};
+			$scope.clear = function() {
+				$scope.searchLog = {};
+			}
+		}]
 	}
 }
